1ab42878ed52887c94a56b5c668338e33918a104
kimu_cloud9 <kimu.000000218@gmail.com>
2020-10-22 08:03:23 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name,  presence: true, length: { maximum: 50 }
@@ -16,6 +17,12 @@ class User < ApplicationRecord
         BCrypt::Password.create(string, cost: cost)
     end
 
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   # ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
